f6e4ab53ded55a46138cf685a3d98afb9707309e
Adityakbr01 <aditykbr01@gmail.com>
2025-12-02 19:36:06 +0530

    Add Day 49 content on DOM manipulation, featuring querySelectorAll and dynamic styling

---
+++ 16/index.js
@@ -0,0 +1,16 @@
+// const allh1 = document.querySelectorAll("h1");
+// console.log(allh1);
+
+// allh1.forEach(function (element, index) {
+//   if (index === 2) {
+//     element.style.color = "red";
+//   }
+// });
+
+
+// allh1[1].style.color = "green";
+
+
+let outerbox = document.querySelector(".outerbox");
+
+console.log(outerbox.childNodes);
